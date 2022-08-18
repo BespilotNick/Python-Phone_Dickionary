c98e55fab1f44b76a6a7b3b321ce91bb7d85710d
NickAllenov <AllenovNS@gmail.com>
2022-08-19 00:43:40 +0300

    Updates 4

--- Python-Phone_Dictionary/Choose_contact.py
+++ Python-Phone_Dictionary/Choose_contact.py
@@ -1,3 +1,54 @@
 
-def coose_contact():
+def checking(param):
     pass
+
+def choose_contact():
+    with open('Phonebook.txt', 'r', encoding='utf-8') as data:
+        text = data.read().split('\n')
+        book = []
+        for row in text:
+            book.append(row)
+        print(book)
+        # for row in book:
+        #     print(row)
+
+    print('Параметры для выбора контакта: '
+          '1 - Порядковый номер. '
+          '2 - Фамилия. '
+          '3 - Имя. '
+          '4 - Номер телефона. '
+          '5 - Комментарий.')
+
+    usernum = input('Выберите параметр: ')
+    params = '12345'
+    while usernum not in params:
+        usernum = input('Введено некорректное значение, выберите параметр из списка: ')
+        continue
+    param = int(usernum)
+
+    if param == 1:
+        index = int(input('Введите порядковый номер контакта: '))
+        contact = [row for row in book if row[0] == index]
+        return contact
+    elif param == 2:
+        surname = input('Введите фамилию контакта: ')
+        contact = [row for row in book if row[1] == surname]
+        return contact
+    elif param == 3:
+        name = input('Введите имя контакта: ')
+        contact = [row for row in book if row[2] == name]
+        return contact
+    elif param == 4:
+        phonenum = input('Введите номер телефона: ')
+        contact = [row for row in book if row[3] == phonenum]
+        return contact
+    elif param == 5:
+        comment = input('Введите комментарий: ')
+        contact = [row for row in book if row[4] == comment]
+        return contact
+    else:
+        return (f"Contact not found")
+
+
+
+choose_contact()
--- Python-Phone_Dictionary/interface.py
+++ Python-Phone_Dictionary/interface.py
@@ -1,19 +1,29 @@
 import Show_dict as shd
 import Add_contact as adc
-import Change_contact as chc
+import Change_contact as cngc
 import Delete_contact as dec
 import Export_dict as expd
 import Import_dict as impd
+import time
 
 def choose_operation():
-    print('Вы находитесь в основном меню, ниже приведены возможные варианты действий:'
-          '1 - Посмотреть телефонную книгу.'
-          '2 - Добавить контакт.'
-          '3 - Редактировать контакт.'
-          '4 - Удалить контакт.'
-          '5 - Импортировать телефонный справочник.'
-          '6 - Экспортировать телефонный справочник.'
-          '7 - Выход.')
+    print('Вы находитесь в основном меню, ниже приведены возможные варианты действий:')
+    time.sleep(0.75)
+    print('1 - Посмотреть телефонную книгу.')
+    time.sleep(0.5)
+    print('2 - Добавить контакт.')
+    time.sleep(0.5)
+    print('3 - Редактировать контакт.')
+    time.sleep(0.5)
+    print('4 - Удалить контакт.')
+    time.sleep(0.5)
+    print('5 - Импортировать данные из телефонного справочника.')
+    time.sleep(0.5)
+    print('6 - Экспортировать данные в телефонный справочник.')
+    time.sleep(0.5)
+    print('7 - Выход.\n')
+    time.sleep(1)
+
     usernum = input('Выберите действие: ')
     actions = '1234567'
     while usernum not in actions:
@@ -25,7 +35,7 @@ def choose_operation():
     elif usernum == 2:
         adc.add_contact()
     elif usernum == 3:
-        chc.change_contact()
+        cngc.change_contact()
     elif usernum == 4:
         dec.delete_contact()
     elif usernum == 5:
@@ -33,4 +43,5 @@ def choose_operation():
     elif usernum == 6:
         expd.export_dict()
     else:
+        print('Всего доброго) Возвращайтесь ещё.')
         exit()
