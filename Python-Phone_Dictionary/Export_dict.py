a414dd0a10206646a5e563d2fa23ef013f30472e
NickAllenov <AllenovNS@gmail.com>
2022-08-19 10:19:07 +0300

    Update 8

--- Python-Phone_Dictionary/Export_dict.py
+++ Python-Phone_Dictionary/Export_dict.py
@@ -7,5 +7,9 @@ def export_to_txt(data):
             data.write(str(row) + '\n')
 
 
-def expor_to_json():
-    pass
+def export_to_json():
+    with open('Phonebook.txt', 'w', encoding='utf-8') as t_book:
+        t_book.read()
+
+    with open('Phonebook.json', 'w') as j_book:
+        json.dump(t_book, j_book)
