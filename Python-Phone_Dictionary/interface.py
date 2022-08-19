c80d2f4eb805c3aac673a9607ad2bbe66faa2922
NickAllenov <AllenovNS@gmail.com>
2022-08-19 12:20:50 +0300

    Update 10

--- Python-Phone_Dictionary/interface.py
+++ Python-Phone_Dictionary/interface.py
@@ -4,25 +4,16 @@ import Change_contact as cngc
 import Delete_contact as dec
 import Export_dict as expd
 import Import_dict as impd
-import time
 
 def choose_operation():
     print('Вы находитесь в основном меню, ниже приведены возможные варианты действий:')
-    time.sleep(0.75)
     print('1 - Посмотреть телефонную книгу.')
-    time.sleep(0.5)
     print('2 - Добавить контакт.')
-    time.sleep(0.5)
     print('3 - Редактировать контакт.')
-    time.sleep(0.5)
     print('4 - Удалить контакт.')
-    time.sleep(0.5)
     print('5 - Импортировать данные из телефонного справочника.')
-    time.sleep(0.5)
     print('6 - Экспортировать данные в телефонный справочник.')
-    time.sleep(0.5)
     print('7 - Выход.\n')
-    time.sleep(1)
 
     usernum = input('Выберите действие: ')
     actions = '1234567'
--- Python-Phone_Dictionary/main.py
+++ Python-Phone_Dictionary/main.py
@@ -1,10 +1,8 @@
 import interface
-import time
 
 def greetings():
     print('Приветствую Вас! Уважаемый пользователь, это телефонный справочник, благодарим Вас за использование нашего'
           ' продукта.\nДалее мы познакомим Вас с возможностями этого справочника.\n')
 
 greetings()
-time.sleep(3)
 interface.choose_operation()
